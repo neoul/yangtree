ead57b5378edc6c48b2e0bc889ae1a91b34a7747
neoul <neoul@ymail.com>
2021-06-19 12:23:12 +0900

    added diff.go

---
+++ diff.go
@@ -0,0 +1,147 @@
+package yangtree
+
+import (
+	"fmt"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+// DiffUpdated() returns updated nodes.
+// It returns all created, replaced nodes in node2 (including itself) against node1.
+// The deleted nodes can be obtained by the reverse input. e.g. node1 ==> node2, node2 ==> node1
+func DiffUpdated(node1, node2 DataNode) ([]DataNode, []DataNode) {
+	if node1 == node2 {
+		return nil, nil
+	}
+	if node1 == nil {
+		created, _ := Find(node2, "...")
+		return created, nil
+	}
+	if node2 == nil {
+		return nil, nil
+	}
+	if node1.Schema() != node2.Schema() {
+		return nil, nil
+	}
+	switch d1 := node1.(type) {
+	case *DataBranch:
+		d2 := node2.(*DataBranch)
+		// created or replaced nodes
+		created := []DataNode{}
+		replaced := []DataNode{}
+		// created, replaced
+		for first := 0; first < len(d2.children); first++ {
+			key := d2.children[first].Key()
+			if IsDuplicatedList(d2.children[first].Schema()) {
+				d1children := d1.GetAll(key)
+				d2children := d2.GetAll(key)
+				for i := range d2children {
+					if i < len(d1children) {
+						c, r := DiffUpdated(d1children[i], d2children[i])
+						created = append(created, c...)
+						replaced = append(replaced, r...)
+					} else {
+						c, r := DiffUpdated(nil, d2children[i])
+						created = append(created, c...)
+						replaced = append(replaced, r...)
+					}
+				}
+				first = len(d2children) - 1
+			} else {
+				c, r := DiffUpdated(d1.Get(key), d2.children[first])
+				created = append(created, c...)
+				replaced = append(replaced, r...)
+			}
+		}
+		return created, replaced
+	case *DataLeaf:
+		d2 := node2.(*DataLeaf)
+		if cmp.Equal(d1.value, d2.value) {
+			return nil, nil
+		}
+		return nil, []DataNode{d2}
+	case *DataLeafList:
+		d2 := node2.(*DataLeafList)
+		if Equal(d1, d2) {
+			return nil, nil
+		}
+		return nil, []DataNode{d2}
+	}
+	return nil, nil
+}
+
+// DiffUpdated() returns updated nodes.
+// It returns all created, replaced nodes in node2 (including itself) against node1.
+// The deleted nodes can be obtained by the reverse input. e.g. node1 ==> node2, node2 ==> node1
+func DiffCreated(node1, node2 DataNode) []DataNode {
+	if node1 == node2 {
+		return nil
+	}
+	if node1 == nil {
+		created, _ := Find(node2, "...")
+		return created
+	}
+	if node2 == nil {
+		return nil
+	}
+	if node1.Schema() != node2.Schema() {
+		return nil
+	}
+	switch d1 := node1.(type) {
+	case *DataBranch:
+		d2 := node2.(*DataBranch)
+		// created nodes
+		created := []DataNode{}
+		// created
+		for first := 0; first < len(d2.children); first++ {
+			key := d2.children[first].Key()
+			if IsDuplicatedList(d2.children[first].Schema()) {
+				d1children := d1.GetAll(key)
+				d2children := d2.GetAll(key)
+				for i := range d2children {
+					if i < len(d1children) {
+						c := DiffCreated(d1children[i], d2children[i])
+						created = append(created, c...)
+					} else {
+						c := DiffCreated(nil, d2children[i])
+						created = append(created, c...)
+					}
+				}
+				first = len(d2children) - 1
+			} else {
+				c := DiffCreated(d1.Get(key), d2.children[first])
+				created = append(created, c...)
+			}
+		}
+		return created
+	case *DataLeaf:
+		return nil
+	case *DataLeafList:
+		return nil
+	}
+	return nil
+}
+
+// Diff() returns differences between nodes.
+// It returns all created, replaced and deleted nodes in node2 (including itself) against node1.
+func Diff(node1, node2 DataNode) ([]DataNode, []DataNode, []DataNode) {
+	c, r := DiffUpdated(node1, node2)
+	d := DiffCreated(node2, node1)
+	return c, r, d
+}
+
+// SetDiff() = Set() + Diff()
+func SetDiff(root DataNode, path string, value ...string) ([]DataNode, []DataNode, error) {
+	if !IsValid(root) {
+		return nil, nil, fmt.Errorf("invalid root node")
+	}
+	new, err := New(root.Schema())
+	if err != nil {
+		return nil, nil, err
+	}
+	if err := Set(new, path, value...); err != nil {
+		return nil, nil, err
+	}
+	c, d := DiffUpdated(root, new)
+	return c, d, nil
+}
---
+++ diff_test.go
@@ -0,0 +1,80 @@
+package yangtree
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDiffUpdate(t *testing.T) {
+	files := []string{
+		"../../YangModels/yang/standard/ietf/RFC/iana-if-type@2017-01-19.yang",
+		"../../openconfig/public/release/models/interfaces/openconfig-interfaces.yang",
+		"../../openconfig/public/release/models/system/openconfig-messages.yang",
+		"../../openconfig/public/release/models/telemetry/openconfig-telemetry.yang",
+		"../../openconfig/public/release/models/openflow/openconfig-openflow.yang",
+		"../../openconfig/public/release/models/platform/openconfig-platform.yang",
+		"../../openconfig/public/release/models/system/openconfig-system.yang",
+	}
+	dir := []string{"../../openconfig/public/", "../../YangModels/yang"}
+	excluded := []string{"ietf-interfaces"}
+	rootschema, err := Load(files, dir, excluded)
+	if err != nil {
+		t.Fatal(err)
+	}
+	root, err := New(rootschema)
+	if err != nil {
+		t.Fatal(err)
+	}
+	schema := FindSchema(rootschema, "interfaces/interface")
+	for i := 1; i < 5; i++ {
+		v := fmt.Sprintf(`{"name":"e%d", "config": {"enabled":"true"}}`, i)
+		new, err := New(schema, v)
+		if err != nil {
+			t.Error(err)
+		}
+		err = Replace(root, "/interfaces/interface", new)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+	prev := Clone(root)
+	for i := 3; i < 7; i++ {
+		v := `{ "config": {"enabled":"false"}, "state": {"admin-status":"DOWN"}}`
+		new, err := New(schema, v)
+		if err != nil {
+			t.Error(err)
+		}
+		err = Replace(root, fmt.Sprintf("interfaces/interface[name=e%v]", i), new)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+	ifnodes, err := Find(root, "interfaces/interface")
+	if err != nil {
+		t.Error(err)
+	}
+	if len(ifnodes) != 6 {
+		t.Errorf("expected num: %d, got: %d", 6, len(ifnodes))
+	}
+	created, replaced := DiffUpdated(prev, root)
+	// created
+	t.Log("[created]", len(created))
+	for i := range created {
+		b, _ := created[i].MarshalJSON()
+		t.Log(created[i].Path(), string(b))
+	}
+	if len(created) != 16 {
+		t.Errorf("expected num: %d, got: %d", 16, len(created))
+	}
+	t.Log("[replaced]", len(replaced))
+	for i := range replaced {
+		b, _ := replaced[i].MarshalJSON()
+		t.Log(replaced[i].Path(), string(b))
+	}
+	if len(replaced) != 2 {
+		t.Errorf("expected num: %d, got: %d", 2, len(replaced))
+	}
+
+	b, _ := root.MarshalJSON()
+	t.Log(string(b))
+}
