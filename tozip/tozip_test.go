e7ee9350cd94e5c48bb8a877df9f69bc5dc56327
neoul <neoul@ymail.com>
2021-08-23 15:11:27 +0900

    updated builtin yang

--- tozip/tozip_test.go
+++ tozip/tozip_test.go
@@ -3,6 +3,7 @@ package tozip
 
 import (
 	"fmt"
+	"os"
 	"testing"
 )
 
@@ -15,8 +16,14 @@ func TestZipUnzip(t *testing.T) {
 		{name: "builtInYangMetadata", file: "../modules/ietf-yang-metadata@2016-08-05.yang"},
 		{name: "builtInYanglib2016", file: "../modules/ietf-yang-library@2016-06-21.yang"},
 		{name: "builtInYanglib2019", file: "../modules/ietf-yang-library@2019-01-04.yang"},
-		{name: "yangtreeRoot", file: "../modules/yangtree.yang"},
+		{name: "builtInYangtreeRoot", file: "../modules/yangtree.yang"},
 	}
+	f, err := os.Create("../builtinyang.go")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+	f.WriteString("package yangtree\n\n")
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			zipped, err := Zip(tt.file)
@@ -24,8 +31,9 @@ func TestZipUnzip(t *testing.T) {
 				t.Errorf("Zip() error = %v", err)
 				return
 			}
-			fmt.Printf("var %s = ", tt.name)
-			fmt.Println(GenerateGoCodes(zipped))
+			f.WriteString(fmt.Sprintf("var %s = ", tt.name))
+			f.WriteString(GenerateGoCodes(zipped))
+			f.WriteString("\n\n")
 			// unzipped, err := Unzip(zipped)
 			// if err != nil {
 			// 	t.Errorf("Unzip() error = %v", err)
